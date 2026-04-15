8f919e992d2c8ab82bb6a4078179d2ed537b179a
2024129004 <0325won@gmail.com>
2026-04-15 15:54:22 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,28 +3,29 @@
 int main(void)
 {
   int sol, num, cnt=0;
-  bool num1 = false;
   
   scanf("%d", sol);
+  scanf("%d", num);
+  
 
   do {
-        scanf("%d", num);
-        num1 = true;
-
-        if (num > sol) {
+        if (num != sol) {
+          if (num > sol) {
            printf("%d>?\n", num);
            cnt++;
-        }
-        if (num < sol) {
+          }
+          if (num < sol) {
            printf("%d<?\n", num);
            cnt++;
+          }
+          scanf("%d", num);
         }
+    
         if (num == sol) {
            printf("%d==?\n", num);
            cnt++;
         }
-          
-  } while(!num1 || num != sol);
+  } while(num != sol);
 
   printf("%d", cnt);
 
